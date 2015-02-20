0f3e449eda4f5ddc727a21aacef904a597a931fd
Michael McCracken <michael.mccracken@gmail.com>
2015-02-20 08:52:38 -0800

    Add mutation and saving, flesh out wrt requirements from review script

--- todotxt/__init__.py
+++ todotxt/__init__.py
@@ -3,9 +3,12 @@
 from collections import defaultdict
 from datetime import datetime
 from functools import total_ordering
+import os
 import re
+import tempfile
 
 DATE_FMT = "%Y-%M-%d"
+
 
 @total_ordering
 class TODO:
@@ -25,7 +28,6 @@ class TODO:
         self.done_date = done_date
         self.contexts = contexts if contexts else []
         self.projects = projects if projects else []
-        self.done = done
 
     def _get_str_components(self):
         d = {}
@@ -39,19 +41,34 @@ class TODO:
         d['text'] = self.text
         if len(self.contexts) + len(self.projects) > 0:
             d['text'] += " "
-        d['cxstr'] = " ".join(["@{}".format(c) for c in sorted(self.contexts)])
-        if len(self.contexts) > 0: d['cxstr'] += " "
-        d['prjstr'] = " ".join(["+{}".format(p) for p in sorted(self.projects)])
-        if len(self.projects) > 0: d['prjstr'] += " "
+        d['cxstr'] = " ".join(["@{}".format(c)
+                               for c in sorted(self.contexts)])
+        if len(self.contexts) > 0:
+            d['cxstr'] += " "
+        d['prjstr'] = " ".join(["+{}".format(p)
+                                for p in sorted(self.projects)])
+        if len(self.projects) > 0:
+            d['prjstr'] += " "
         return d
 
     def __str__(self):
         d = self._get_str_components()
-        return "'{donestr}'{pstr}{cdstr}{text}{prjstr}{cxstr}".format(**d)
+        return "{donestr}{pstr}{cdstr}{text}{prjstr}{cxstr}".format(**d)
 
     def __eq__(self, other):
         # depends on sorted context/project arrays
         return str(self) == str(other)
+
+    @property
+    def done(self):
+        return self.done_date is not None
+
+    @done.setter
+    def done(self, newval):
+        if newval:
+            self.done_date = datetime.strftime(datetime.now(), DATE_FMT)
+        else:
+            self.done_date = None
 
     def __lt__(self, other):
         def get_tuple(d):
@@ -64,6 +81,7 @@ class TODO:
         d_self = self._get_str_components()
         d_other = other._get_str_components()
         return get_tuple(d_self) < get_tuple(d_other)
+
 
 def todo_from_line(line):
     """returns a TODO object parsed from 'line'"""
@@ -91,11 +109,12 @@ def todo_from_line(line):
     t_created_date = None
     try:
         d = datetime.strptime(words[0], DATE_FMT)
+        d  # flakes :(
         t_created_date = words[0]
         words = words[1:]
-    except ValueError as ve:
+    except ValueError:
         pass
-    
+
     t_projects = []
     t_contexts = []
     t_txt = []
@@ -109,29 +128,62 @@ def todo_from_line(line):
     t_txt = " ".join(t_txt)
 
     return TODO(t_txt, priority=t_prio,
-                created_date = t_created_date,
-                done_date = t_done_date,
+                created_date=t_created_date,
+                done_date=t_done_date,
                 contexts=t_contexts,
                 projects=t_projects,
                 done=t_done)
-    
-        
+
+
 class TODOFile:
     def __init__(self, filename):
         self.filename = filename
-        self.todos = []
+        self._todos = []
         with open(self.filename, 'r') as f:
-            self.todos += [todo_from_line(l) for l in f.readlines()]
-        self.todos = [t for t in self.todos if t is not None]
+            self._todos += [todo_from_line(l) for l in f.readlines()]
+        self._todos = [t for t in self._todos if t is not None]
 
+        self._recalc()
+
+    def __str__(self):
+        return "\n".join(map(str, self._todos))
+
+    def _recalc(self):
         self.projects = defaultdict(list)
         self.contexts = defaultdict(list)
 
-        for t in self.todos:
+        for t in self._todos:
             for p in t.projects:
                 self.projects[p].append(t)
             for c in t.contexts:
                 self.contexts[c].append(t)
-        
-    def __str__(self):
-        return "\n".join(map(str, self.todos))
+
+    def summary(self):
+        return ("{}: {} todos, {} projects and "
+                "{} contexts".format(os.path.basename(self.filename),
+                                     len(self._todos),
+                                     len(self.projects),
+                                     len(self.contexts)))
+
+    def add_todo(self, todo):
+        self._todos.append(todo)
+        self._recalc()
+
+    def delete_todo(self, todo):
+        self._todos.remove(todo)
+        self._recalc()
+
+    def replace_todo(self, old, new):
+        self._todos[self._todos.index(old)] = new
+        self._recalc()
+
+    def get_todos(self):
+        return self._todos
+
+    def save(self):
+        with tempfile.NamedTemporaryFile(
+                dir=os.path.dirname(self.filename),
+                delete=False) as fout:
+            fout.write(str(self).encode())
+            fout.write("\n".encode())
+        os.replace(fout.name, self.filename)
